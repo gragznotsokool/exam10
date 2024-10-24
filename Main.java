14c98e5d754fa0200eab3a5607625d0104c32cc5
gragzz <gragz2024@gmail.com>
2024-10-25 01:25:38 +0530

    greeting added

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+// Main.java
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Welcome to Git Version Control!");
     }
 }
